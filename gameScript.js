e4b95080e31179410ebe8ce1b5d3b6d7c390f480
Markus Bengtsson <ma7665be-s@student.lu.se>
2020-12-15 18:21:11 +0100

    ja

--- gameScript.js
+++ gameScript.js
@@ -1,76 +1,60 @@
+const pi = BABYLON.Scalar.TwoPi / 2;
 
-const pi = BABYLON.Scalar.TwoPi/2;
-
-window.addEventListener("DOMContentLoaded", function(){
-  var canvas = document.getElementById("canvas");
-  var engine = new BABYLON.Engine(canvas, true);
-  var canvasInvRes = new BABYLON.Vector2(1/canvas.width,1/canvas.height);
+var canvas = document.getElementById("canvas");
+var engine = new BABYLON.Engine(canvas, true);
 
 
-  var createScene = function(){
-    var scene = new BABYLON.Scene(engine);
 
-    scene.clearColor = new BABYLON.Color3(61/255, 59/255, 61/255);
-    var camera = new BABYLON.ArcRotateCamera("Camera", pi/2, pi/2, 5, new BABYLON.Vector3(0, 0, 0), scene);
-    camera.setTarget(BABYLON.Vector3.Zero());
-    camera.attachControl(canvas, true);
-    camera.keysUp.push(87);
-    camera.keysDown.push(83);
-    camera.keysLeft.push(65);
-    camera.keysRight.push(68);
-    var depthrend = scene.enableDepthRenderer();
-    var georend = scene.enableGeometryBufferRenderer();
+var createScene = function() {
+  var scene = new BABYLON.Scene(engine);
 
-    var shaderMaterial = new BABYLON.ShaderMaterial("shader", scene, {
-        vertexElement: "vertexShaderCode",
-        fragmentElement: "fragmentShaderCode",
-    },
-    {
-        attributes: ["position", "normal", "uv"],
-        uniforms: ["world", "worldView", "worldViewProjection", "view", "projection"]
-    });
+  scene.clearColor = new BABYLON.Color3(0.05,0.05,0.09);
+  var camera = new BABYLON.ArcRotateCamera("Camera", pi / 2, pi / 2, 5, new BABYLON.Vector3(0, 0, 0), scene);
+  camera.setTarget(BABYLON.Vector3.Zero());
+  camera.attachControl(canvas, true);
+  camera.keysUp.push(87);
+  camera.keysDown.push(83);
+  camera.keysLeft.push(65);
+  camera.keysRight.push(68);
+  var depthRend = scene.enableDepthRenderer();
+  var geoRend = scene.enableGeometryBufferRenderer();
 
-    //var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 10, -5), scene);
-    var light = new BABYLON.SpotLight("spotLight", new BABYLON.Vector3(1, 5, 1), new BABYLON.Vector3(-1, -5, -1), pi/3, 1, scene);
-    BABYLON.SceneLoader.ImportMesh("","/", "louvre-demosthenes-photoscan.obj", scene, function (newMeshes) {
-    // do something with the scene
+  var shaderMaterial = new BABYLON.ShaderMaterial("shader", scene, {
+    vertexElement: "vertexShaderCode",
+    fragmentElement: "fragmentShaderCode",
+  }, {
+    attributes: ["position", "normal", "uv"],
+    uniforms: ["world", "worldView", "worldViewProjection", "view", "projection"]
+  });
+
+
+  var lightPosition = new BABYLON.Vector3(1, 5, 1);
+  BABYLON.SceneLoader.ImportMesh("", "/", "louvre-demosthenes-photoscan.obj", scene, function(newMeshes) {
 
     var mesh = newMeshes[0];
-
+    console.log(mesh);
     mesh.material = shaderMaterial;
 
-    mesh.rotation.z += pi/2;
+    mesh.rotation.z += pi / 2;
     mesh.position.z -= 5;
-    });
-
-    light.specular = BABYLON.Color3.Red();
-
-    //var box = BABYLON.MeshBuilder.CreateBox("box",{size:4.0},scene);
-    var materialBox = new BABYLON.StandardMaterial("materialBox", scene);
-    materialBox.specularColor = BABYLON.Color3.Red();
-    materialBox.diffuseColor = BABYLON.Color3.Yellow();
-    //box.material = shaderMaterial;
-
-
-    //camera.parent = player;
-    scene.onBeforeRenderObservable.add(()=> {
-      //box.rotation.y += 0.01;
-      //box.rotation.x += 0.01;
-      //box.rotation.z += 0.01;
-      shaderMaterial.setVector3("lightposition", light.position);
-      shaderMaterial.setVector3("lightdirection", light.direction);
-      shaderMaterial.setVector3("cameraposition", camera.position);
-      shaderMaterial.setVector2("invRes", canvasInvRes);
-      shaderMaterial.setTexture("depthmap", depthrend.getDepthMap());
-      shaderMaterial.setTexture("normalmap", georend.getGBuffer().textures[1]);
-
-    });
-
-    return scene;
-  }
-
-  var scene = createScene();
-  engine.runRenderLoop(function(){
-    scene.render();
   });
+
+  scene.onBeforeRenderObservable.add(() => {
+    shaderMaterial.setVector3("lightPosition", lightPosition);
+    shaderMaterial.setVector3("cameraPosition", camera.position);
+    var canvasInvRes = new BABYLON.Vector2(1 / canvas.width, 1 / canvas.height);
+    shaderMaterial.setVector2("invRes", canvasInvRes);
+    shaderMaterial.setTexture("depthmap", depthRend.getDepthMap());
+    shaderMaterial.setTexture("normalmap", geoRend.getGBuffer().textures[1]);
+  });
+  return scene;
+}
+
+var scene = createScene();
+engine.runRenderLoop(function() {
+  scene.render();
+});
+
+window.addEventListener("resize", function () {
+        engine.resize();
 });
